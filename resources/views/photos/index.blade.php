62f37a5cf6f2085e89000167f8432651f01141e0
Lucas Ruiz <yacogamer03@gmail.com>
2025-06-05 17:52:15 -0500

    segundo commit

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -2,8 +2,8 @@
     <div> <h3>{{$photo->title}}</h3>
     <img src="{{$photo->url }}" width ="200">
     <p>Etiquetas:
-    @foreach($photos ->tags as $tag)
-    <span>{{ $tag-name}}</span>
+    @foreach($photo->tags as $tag)
+    <span>{{ $tag->name}}</span>
     @endforeach </p>
     </div>
 @endforeach
